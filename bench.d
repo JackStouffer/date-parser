266c4a66d4463b48cdb32e4f676ea0156a5be6cf
Jack Stouffer <jack@jackstouffer.com>
2016-04-02 23:57:24 -0400

    refactored benchmark code to be more accurate

--- bench.d
+++ bench.d
@@ -19,29 +19,9 @@ enum stringThree = "2003-09-25";
 enum stringFour = "2003-09-25T10:49:41.5-03:00";
 enum stringFive = "25-Sep-2003";
 
-void parse_test()
+SysTime parse_test(string val)
 {
-    auto a = parse(stringOne);
-}
-
-void parse_test2()
-{
-    auto a = parse(stringTwo);
-}
-
-void parse_test3()
-{
-    auto a = parse(stringThree);
-}
-
-void parse_test4()
-{
-    auto a = parse(stringFour);
-}
-
-void parse_test5()
-{
-    auto a = parse(stringFive);
+    return parse(val);
 }
 
 void main()
@@ -56,12 +36,11 @@ void main()
             theAllocator(a);
         }
 
-        auto r = benchmark!(parse_test, parse_test2, parse_test3, parse_test4, parse_test5)(testCount);
-        auto result = to!Duration(r[0] / testCount);
-        auto result2 = to!Duration(r[1] / testCount);
-        auto result3 = to!Duration(r[2] / testCount);
-        auto result4 = to!Duration(r[3] / testCount);
-        auto result5 = to!Duration(r[4] / testCount);
+        auto result = to!Duration(benchmark!(() => parse_test(stringOne))(testCount)[0] / testCount);
+        auto result2 = to!Duration(benchmark!(() => parse_test(stringTwo))(testCount)[0] / testCount);
+        auto result3 = to!Duration(benchmark!(() => parse_test(stringThree))(testCount)[0] / testCount);
+        auto result4 = to!Duration(benchmark!(() => parse_test(stringFour))(testCount)[0] / testCount);
+        auto result5 = to!Duration(benchmark!(() => parse_test(stringFive))(testCount)[0] / testCount);
 
         writeln(stringOne, "\t", result);
         writeln(stringTwo, "\t\t\t", result2);
