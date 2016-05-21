fcc8a3aa7767b253e87cdf0986fcacec3e3ff879
Jack Stouffer <jack@jackstouffer.com>
2016-05-21 19:16:41 -0400

    Move Voldemort types out of function bodies to avoid Phobos Issue 15831

--- src/dateparser.d
+++ src/dateparser.d
@@ -132,92 +132,91 @@ static if (version_major == 2 && version_minor < 72)
     auto splitterWithMatches(Range, RegEx)(Range r, RegEx pat) if (
             is(Unqual!(ElementEncodingType!Range) : dchar))
     {
-        /++
-        Range that splits a string using a regular expression as a
-        separator.
-        +/
-        static struct Result(Range, alias RegEx = Regex)
+        return SplitterResult!(Range, RegEx)(r, pat);
+    }
+
+    // Issue 15831: This should be a Voldemort type, but due to linker slowdown
+    // it's a good idea to put this outside so we don't slowdown people's build
+    // times
+    static struct SplitterResult(Range, alias RegEx = Regex)
+    {
+    private:
+        Range _input;
+        size_t _offset;
+        bool onMatch = false;
+        alias Rx = typeof(match(Range.init, RegEx.init));
+        Rx _match;
+
+        @trusted this(Range input, RegEx separator)
         {
-        private:
-            Range _input;
-            size_t _offset;
-            bool onMatch = false;
-            alias Rx = typeof(match(Range.init, RegEx.init));
-            Rx _match;
-
-            @trusted this(Range input, RegEx separator)
+            _input = input;
+            if (_input.empty)
             {
-                _input = input;
-                if (_input.empty)
-                {
-                    //there is nothing to match at all, make _offset > 0
-                    _offset = 1;
-                }
-                else
-                {
-                    _match = Rx(_input, separator);
-                }
+                //there is nothing to match at all, make _offset > 0
+                _offset = 1;
             }
-
-        public:
-            auto ref opSlice()
+            else
             {
-                return this.save;
-            }
-
-            ///Forward range primitives.
-            @property Range front()
-            {
-                import std.algorithm : min;
-
-                assert(!empty && _offset <= _match.pre.length && _match.pre.length <= _input.length);
-
-                if (!onMatch)
-                    return _input[_offset .. min($, _match.pre.length)];
-                else
-                    return _match.hit();
-            }
-
-            ///ditto
-            @property bool empty()
-            {
-                return _offset >= _input.length;
-            }
-
-            ///ditto
-            void popFront()
-            {
-                assert(!empty);
-                if (_match.empty)
-                {
-                    //No more separators, work is done here
-                    _offset = _input.length + 1;
-                }
-                else
-                {
-                    if (!onMatch)
-                    {
-                        //skip past the separator
-                        _offset = _match.pre.length;
-                        onMatch = true;
-                    }
-                    else
-                    {
-                        onMatch = false;
-                        _offset += _match.hit.length;
-                        _match.popFront();
-                    }
-                }
-            }
-
-            ///ditto
-            @property auto save()
-            {
-                return this;
+                _match = Rx(_input, separator);
             }
         }
 
-        return Result!(Range, RegEx)(r, pat);
+    public:
+        auto ref opSlice()
+        {
+            return this.save;
+        }
+
+        ///Forward range primitives.
+        @property Range front()
+        {
+            import std.algorithm : min;
+
+            assert(!empty && _offset <= _match.pre.length && _match.pre.length <= _input.length);
+
+            if (!onMatch)
+                return _input[_offset .. min($, _match.pre.length)];
+            else
+                return _match.hit();
+        }
+
+        ///ditto
+        @property bool empty()
+        {
+            return _offset >= _input.length;
+        }
+
+        ///ditto
+        void popFront()
+        {
+            assert(!empty);
+            if (_match.empty)
+            {
+                //No more separators, work is done here
+                _offset = _input.length + 1;
+            }
+            else
+            {
+                if (!onMatch)
+                {
+                    //skip past the separator
+                    _offset = _match.pre.length;
+                    onMatch = true;
+                }
+                else
+                {
+                    onMatch = false;
+                    _offset += _match.hit.length;
+                    _match.popFront();
+                }
+            }
+        }
+
+        ///ditto
+        @property auto save()
+        {
+            return this;
+        }
     }
 }
 else
@@ -225,7 +224,6 @@ else
     alias splitterWithMatches = splitter!(Yes.keepSeparators);
 }
 
-///
 unittest
 {
     import std.algorithm.comparison : equal;
@@ -259,198 +257,201 @@ unittest
 */
 auto timeLexer(Range)(Range r) if (isInputRange!Range && isSomeChar!(ElementEncodingType!Range))
 {
-    static struct Result
+    return TimeLexerResult!Range(r);
+}
+
+// Issue 15831: This should be a Voldemort type, but due to linker slowdown
+// it's a good idea to put this outside so we don't slowdown people's build
+// times
+struct TimeLexerResult(Range)
+{
+private:
+    Range source;
+    string charStack;
+    string[] tokenStack;
+    string token;
+    enum State
     {
-    private:
-        Range source;
-        string charStack;
-        string[] tokenStack;
-        string token;
-        enum State
+        EMPTY,
+        ALPHA,
+        NUMERIC,
+        ALPHA_PERIOD,
+        PERIOD,
+        NUMERIC_PERIOD
+    }
+
+public:
+    this(Range r)
+    {
+        source = r;
+        popFront();
+    }
+
+    auto front() @property
+    {
+        return token;
+    }
+
+    void popFront()
+    {
+        import std.string : count;
+        import std.algorithm.searching : canFind;
+        import std.uni : isNumber, isSpace, isAlpha;
+
+        if (tokenStack.length > 0)
         {
-            EMPTY,
-            ALPHA,
-            NUMERIC,
-            ALPHA_PERIOD,
-            PERIOD,
-            NUMERIC_PERIOD
+            immutable f = tokenStack.front;
+            tokenStack.popFront;
+            token = f;
+            return;
         }
 
-    public:
-        this(Range r)
-        {
-            source = r;
-            popFront();
-        }
+        bool seenLetters = false;
+        State state = State.EMPTY;
+        token = string.init;
 
-        auto front() @property
+        while (!source.empty || !charStack.empty)
         {
-            return token;
-        }
+            // We only realize that we've reached the end of a token when we
+            // find a character that's not part of the current token - since
+            // that character may be part of the next token, it's stored in the
+            // charStack.
+            dchar nextChar;
 
-        void popFront()
-        {
-            import std.string : count;
-            import std.algorithm.searching : canFind;
-            import std.uni : isNumber, isSpace, isAlpha;
-
-            if (tokenStack.length > 0)
+            if (!charStack.empty)
             {
-                immutable f = tokenStack.front;
-                tokenStack.popFront;
-                token = f;
-                return;
+                nextChar = charStack.front;
+                charStack.popFront;
+            }
+            else
+            {
+                nextChar = source.front;
+                source.popFront;
             }
 
-            bool seenLetters = false;
-            State state = State.EMPTY;
-            token = string.init;
-
-            while (!source.empty || !charStack.empty)
+            if (state == State.EMPTY)
             {
-                // We only realize that we've reached the end of a token when we
-                // find a character that's not part of the current token - since
-                // that character may be part of the next token, it's stored in the
-                // charStack.
-                dchar nextChar;
+                debug(dateparser) writeln("EMPTY");
+                // First character of the token - determines if we're starting
+                // to parse a word, a number or something else.
+                token ~= nextChar;
 
-                if (!charStack.empty)
+                if (nextChar.isAlpha)
+                    state = State.ALPHA;
+                else if (nextChar.isNumber)
+                    state = State.NUMERIC;
+                else if (nextChar.isSpace)
                 {
-                    nextChar = charStack.front;
-                    charStack.popFront;
+                    token = " ";
+                    break; //emit token
+                }
+                else
+                    break; //emit token
+                debug(dateparser) writeln("TOKEN ", token, " STATE ", state);
+            }
+            else if (state == State.ALPHA)
+            {
+                debug(dateparser) writeln("STATE ", state, " nextChar: ", nextChar);
+                // If we've already started reading a word, we keep reading
+                // letters until we find something that's not part of a word.
+                seenLetters = true;
+
+                if (nextChar.isAlpha)
+                    token ~= nextChar;
+                else if (nextChar == '.')
+                {
+                    token ~= nextChar;
+                    state = State.ALPHA_PERIOD;
                 }
                 else
                 {
-                    nextChar = source.front;
-                    source.popFront;
-                }
-
-                if (state == State.EMPTY)
-                {
-                    debug(dateparser) writeln("EMPTY");
-                    // First character of the token - determines if we're starting
-                    // to parse a word, a number or something else.
-                    token ~= nextChar;
-
-                    if (nextChar.isAlpha)
-                        state = State.ALPHA;
-                    else if (nextChar.isNumber)
-                        state = State.NUMERIC;
-                    else if (nextChar.isSpace)
-                    {
-                        token = " ";
-                        break; //emit token
-                    }
-                    else
-                        break; //emit token
-                    debug(dateparser) writeln("TOKEN ", token, " STATE ", state);
-                }
-                else if (state == State.ALPHA)
-                {
-                    debug(dateparser) writeln("STATE ", state, " nextChar: ", nextChar);
-                    // If we've already started reading a word, we keep reading
-                    // letters until we find something that's not part of a word.
-                    seenLetters = true;
-
-                    if (nextChar.isAlpha)
-                        token ~= nextChar;
-                    else if (nextChar == '.')
-                    {
-                        token ~= nextChar;
-                        state = State.ALPHA_PERIOD;
-                    }
-                    else
-                    {
-                        charStack ~= nextChar;
-                        break; //emit token
-                    }
-                }
-                else if (state == State.NUMERIC)
-                {
-                    // If we've already started reading a number, we keep reading
-                    // numbers until we find something that doesn't fit.
-                    debug(dateparser) writeln("STATE ", state, " nextChar: ", nextChar);
-                    if (nextChar.isNumber)
-                        token ~= nextChar;
-                    else if (nextChar == '.' || (nextChar == ',' && token.length >= 2))
-                    {
-                        token ~= nextChar;
-                        state = State.NUMERIC_PERIOD;
-                    }
-                    else
-                    {
-                        charStack ~= nextChar;
-                        debug(dateparser) writeln("charStack add: ", charStack);
-                        break; //emit token
-                    }
-                }
-                else if (state == State.ALPHA_PERIOD)
-                {
-                    debug(dateparser) writeln("STATE ", state, " nextChar: ", nextChar);
-                    // If we've seen some letters and a dot separator, continue
-                    // parsing, and the tokens will be broken up later.
-                    seenLetters = true;
-                    if (nextChar == '.' || nextChar.isAlpha)
-                        token ~= nextChar;
-                    else if (nextChar.isNumber && token[$ - 1] == '.')
-                    {
-                        token ~= nextChar;
-                        state = State.NUMERIC_PERIOD;
-                    }
-                    else
-                    {
-                        charStack ~= nextChar;
-                        break; //emit token
-                    }
-                }
-                else if (state == State.NUMERIC_PERIOD)
-                {
-                    debug(dateparser) writeln("STATE ", state, " nextChar: ", nextChar);
-                    // If we've seen at least one dot separator, keep going, we'll
-                    // break up the tokens later.
-                    if (nextChar == '.' || nextChar.isNumber)
-                        token ~= nextChar;
-                    else if (nextChar.isAlpha && token[$ - 1] == '.')
-                    {
-                        token ~= nextChar;
-                        state = State.ALPHA_PERIOD;
-                    }
-                    else
-                    {
-                        charStack ~= nextChar;
-                        break; //emit token
-                    }
+                    charStack ~= nextChar;
+                    break; //emit token
                 }
             }
-
-            debug(dateparser) writeln("STATE ", state, " seenLetters: ", seenLetters);
-            if ((state == State.ALPHA_PERIOD || state == State.NUMERIC_PERIOD)
-                    && (seenLetters || token.count('.') > 1
-                    || (token[$ - 1] == '.' || token[$ - 1] == ',')))
-                if ((state == State.ALPHA_PERIOD
-                        || state == State.NUMERIC_PERIOD) && (seenLetters
-                        || token.count('.') > 1 || (token[$ - 1] == '.' || token[$ - 1] == ',')))
+            else if (state == State.NUMERIC)
+            {
+                // If we've already started reading a number, we keep reading
+                // numbers until we find something that doesn't fit.
+                debug(dateparser) writeln("STATE ", state, " nextChar: ", nextChar);
+                if (nextChar.isNumber)
+                    token ~= nextChar;
+                else if (nextChar == '.' || (nextChar == ',' && token.length >= 2))
                 {
-                    auto l = splitterWithMatches(token[], split_decimal);
-                    token = l.front;
-                    l.popFront;
-
-                    foreach (tok; l)
-                        if (tok.length > 0)
-                            tokenStack ~= tok;
+                    token ~= nextChar;
+                    state = State.NUMERIC_PERIOD;
                 }
-
-            if (state == State.NUMERIC_PERIOD && !token.canFind('.'))
-                token = token.replace(",", ".");
+                else
+                {
+                    charStack ~= nextChar;
+                    debug(dateparser) writeln("charStack add: ", charStack);
+                    break; //emit token
+                }
+            }
+            else if (state == State.ALPHA_PERIOD)
+            {
+                debug(dateparser) writeln("STATE ", state, " nextChar: ", nextChar);
+                // If we've seen some letters and a dot separator, continue
+                // parsing, and the tokens will be broken up later.
+                seenLetters = true;
+                if (nextChar == '.' || nextChar.isAlpha)
+                    token ~= nextChar;
+                else if (nextChar.isNumber && token[$ - 1] == '.')
+                {
+                    token ~= nextChar;
+                    state = State.NUMERIC_PERIOD;
+                }
+                else
+                {
+                    charStack ~= nextChar;
+                    break; //emit token
+                }
+            }
+            else if (state == State.NUMERIC_PERIOD)
+            {
+                debug(dateparser) writeln("STATE ", state, " nextChar: ", nextChar);
+                // If we've seen at least one dot separator, keep going, we'll
+                // break up the tokens later.
+                if (nextChar == '.' || nextChar.isNumber)
+                    token ~= nextChar;
+                else if (nextChar.isAlpha && token[$ - 1] == '.')
+                {
+                    token ~= nextChar;
+                    state = State.ALPHA_PERIOD;
+                }
+                else
+                {
+                    charStack ~= nextChar;
+                    break; //emit token
+                }
+            }
         }
 
-        bool empty()() @property
-        {
-            return token.empty && source.empty && charStack.empty && tokenStack.empty;
-        }
+        debug(dateparser) writeln("STATE ", state, " seenLetters: ", seenLetters);
+        if ((state == State.ALPHA_PERIOD || state == State.NUMERIC_PERIOD)
+                && (seenLetters || token.count('.') > 1
+                || (token[$ - 1] == '.' || token[$ - 1] == ',')))
+            if ((state == State.ALPHA_PERIOD
+                    || state == State.NUMERIC_PERIOD) && (seenLetters
+                    || token.count('.') > 1 || (token[$ - 1] == '.' || token[$ - 1] == ',')))
+            {
+                auto l = splitterWithMatches(token[], split_decimal);
+                token = l.front;
+                l.popFront;
+
+                foreach (tok; l)
+                    if (tok.length > 0)
+                        tokenStack ~= tok;
+            }
+
+        if (state == State.NUMERIC_PERIOD && !token.canFind('.'))
+            token = token.replace(",", ".");
     }
 
-    return Result(r);
+    bool empty()() @property
+    {
+        return token.empty && source.empty && charStack.empty && tokenStack.empty;
+    }
 }
 
 unittest
